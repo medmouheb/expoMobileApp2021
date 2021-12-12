31b210ac2471055e9e0a37cf287a5bd956a9b181
medmouheb <mohamedmouheb@gmail.com>
2021-12-12 01:00:39 +0100

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import MapView from 'react-native-maps';
+import { Button, View, Text, Dimensions, StyleSheet, SafeAreaView, SectionList, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+      <Button
+        title="Go to map"
+        onPress={() => navigation.navigate('MapApp')}
+      />
+      <Button
+        title="Go to ListOfItems"
+        onPress={() => navigation.navigate('ListOfItems')}
+      />
+    </View>
+  );
+}
+const DATA = [
+  {
+    title: 'Main dishes',
+    data: ['Pizza', 'Burger', 'Risotto'],
+  },
+  {
+    title: 'Sides',
+    data: ['French Fries', 'Onion Rings', 'Fried Shrimps'],
+  },
+  {
+    title: 'Drinks',
+    data: ['Water', 'Coke', 'Beer'],
+  },
+  {
+    title: 'Desserts',
+    data: ['Cheese Cake', 'Ice Cream'],
+  },
+];
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+const ListOfItems = () => (
+  
+  <SafeAreaView style={styles.container}>
+    <SectionList
+      sections={DATA}
+      keyExtractor={(item, index) => item + index}
+      renderItem={({ item }) => <Item title={item} />}
+      renderSectionHeader={({ section: { title } }) => <Text style={styles.header}>{title}</Text>}
+    />
+
+
+  </SafeAreaView>
+  
+  
+);
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      {/* <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      /> */}
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      {/* <Button title="Go back" onPress={() => navigation.goBack()} /> */}
+    </View>
+  );
+}
+
+function MapApp() {
+  return (
+    <View style={styles.mapContainer}>
+      <MapView style={styles.map} />
     </View>
   );
 }
@@ -14,8 +88,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: StatusBar.currentHeight,
+    marginHorizontal: 16,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+  },
+  header: {
+    fontSize: 32,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+  },
+  mapContainer: {
+    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
 });
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="MapApp" component={MapApp} />
+        <Stack.Screen name="ListOfItems" component={ListOfItems} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
